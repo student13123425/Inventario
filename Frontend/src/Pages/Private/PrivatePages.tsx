474332ee3dd4de6c7bc23104d653b2a638a012c1
student13123425 <nicolae.m.mihaialexandru24@stud.rau.ro>
2025-11-29 21:52:26 +0200

    Add stubs for private page components
    Created placeholder components for InventoryManagement, ProductCatalogue, and Transactions. Updated PrivatePages to dynamically render these components based on navigation.

--- Frontend/src/Pages/Private/PrivatePages.tsx
+++ Frontend/src/Pages/Private/PrivatePages.tsx
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import Navbar from './Navbar';
 import DashBoard from './DashBoard';
+import ManageSupplyers from './ManageSupplyers';
+import ProductCatalogue from './ProductCatalogue';
+import InventoryManagement from './InventoryManagement';
+import Transactions from './Transactions';
 
 const PageContainer=styled.div`
   width: 100vw;
@@ -11,11 +15,13 @@ const PageContainer=styled.div`
 // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export default function PrivatePages(props:{setError:Function,LoginToken:string, onLogout:Function}) {
   const [CurrentPage,setCurrentPage]=useState<number>(0)
+  const Elements=[DashBoard,ManageSupplyers,ProductCatalogue,InventoryManagement,Transactions]
+  const Element=Elements[CurrentPage]
   return (
       <>
         <Navbar setCurrentPage={setCurrentPage} Logout={props.onLogout}/>
         <PageContainer>
-          <DashBoard setError={props.setError}/>
+          <Element setError={props.setError}/>
         </PageContainer>
       </>
   );
