f80c8f23fc4481fe3e7da5813999a540caa30959
student13123425 <nicolae.m.mihaialexandru24@stud.rau.ro>
2025-11-29 12:20:02 +0200

    Add Navbar component to private pages
    Introduced a new Navbar component with basic styling and integrated it into PrivatePages. Also added a styled PageContainer to structure the layout below the navbar.

--- Frontend/src/Pages/Private/PrivatePages.tsx
+++ Frontend/src/Pages/Private/PrivatePages.tsx
@@ -1,9 +1,19 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import Navbar from './Navbar';
 
+const PageContainer=styled.div`
+  width: 100vw;
+  height: calc(100vh - 3rem);
+`
 
 export default function PrivatePages({ LoginToken, onLogout }: PrivatePagesProps) {
   return (
-      <></>
+      <>
+        <Navbar/>
+        <PageContainer>
+
+        </PageContainer>
+      </>
   );
 }
