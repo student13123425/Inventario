f816fb527722a52e395085bc9fcaad3defe1eb42
student13123425 <nicolae.m.mihaialexandru24@stud.rau.ro>
2025-11-29 21:59:33 +0200

    Add styled layout containers to private pages
    Introduced styled-components for consistent layout in InventoryManagement, ManageSupplyers, ProductCatalogue, and Transactions pages. Each page now uses Container and ContainerInner components for improved structure and spacing.

--- Frontend/src/Pages/Private/ProductCatalogue.tsx
+++ Frontend/src/Pages/Private/ProductCatalogue.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100%;
+`
+
+const ContainerInner = styled.div`
+  margin: auto;
+  width: 1000px;
+  max-width: 100vw;
+  display: flex;
+  padding: 1rem;
+  gap: 1rem;
+`
+
 
 export default function ProductCatalogue() {
   return (
-    <div>ProductCatalogue</div>
+    <Container>
+      <ContainerInner>
+        
+      </ContainerInner>
+    </Container>
   )
 }
