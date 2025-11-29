cff2614784ae88e9663e747cbf894ae18beacdf5
student13123425 <nicolae.m.mihaialexandru24@stud.rau.ro>
2025-11-29 22:14:03 +0200

    Add product fetching logic to ProductCatalogue
    ProductCatalogue now fetches products using fetchProducts and displays a loading component while data is being retrieved. Error handling is added via setError, and the component uses useEffect and useState for data management.

--- Frontend/src/Pages/Private/ProductCatalogue.tsx
+++ Frontend/src/Pages/Private/ProductCatalogue.tsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import { fetchProducts } from '../../script/network'
+import { getToken } from '../../script/utils'
+import type { ProductResponse } from '../../script/objects'
+import LoadingComponent from './LoadingComponent'
 
 const Container = styled.div`
   width: 100vw;
@@ -15,12 +19,36 @@ const ContainerInner = styled.div`
   gap: 1rem;
 `
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+async function getProducts(setProducts: Function, setError: Function) {
+  try {
+    const token = await getToken()
+    if (token) {
+      const out = await fetchProducts(token)
+      if (out.success == true)
+        setProducts(out.products)
+      else
+        setError("Failed getting products")
+    }
+  } catch (e) {
+    setError("Failed getting products")
+  }
+}
 
-export default function ProductCatalogue() {
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+export default function ProductCatalogue(props: { setError: Function }) {
+  const [Products, setProducts] = useState<ProductResponse[] | null>(null)
+  
+  useEffect(() => {
+    getProducts(setProducts, props.setError)
+  }, [props.setError])
+
+  if (Products === null)
+    return <LoadingComponent msg='Loading Products...' />
+
   return (
     <Container>
       <ContainerInner>
-        
       </ContainerInner>
     </Container>
   )
