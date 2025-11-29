0c34c9e979cde75957ed5dd6fa643e2f55070f4f
student13123425 <nicolae.m.mihaialexandru24@stud.rau.ro>
2025-11-29 22:20:18 +0200

    Add inventory data fetching to InventoryManagement
    InventoryManagement now fetches products and their stock levels using network utilities and displays a loading component while data is being retrieved. Error handling has been added, and the component now accepts a setError prop for reporting issues.

--- Frontend/src/Pages/Private/InventoryManagement.tsx
+++ Frontend/src/Pages/Private/InventoryManagement.tsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import { fetchProducts, fetchStockLevel } from '../../script/network'
+import { getToken } from '../../script/utils'
+import type { ProductResponse, StockLevelResponse } from '../../script/objects'
+import LoadingComponent from './LoadingComponent'
 
 const Container = styled.div`
   width: 100vw;
@@ -15,12 +19,58 @@ const ContainerInner = styled.div`
   gap: 1rem;
 `
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+async function getInventoryData(setInventory: Function, setError: Function) {
+  try {
+    const token = await getToken()
+    if (token) {
+      // First fetch all products
+      const productsOut = await fetchProducts(token)
+      if (productsOut.success == true) {
+        // Then fetch stock levels for each product
+        const inventoryWithStock = await Promise.all(
+          productsOut.products.map(async (product: ProductResponse) => {
+            try {
+              const stockOut = await fetchStockLevel(token, product.id)
+              return {
+                product,
+                stockLevel: stockOut
+              }
+            } catch (e) {
+              return {
+                product,
+                stockLevel: { success: false, productId: product.id, quantity: 0 }
+              }
+            }
+          })
+        )
+        setInventory(inventoryWithStock)
+      } else {
+        setError("Failed getting products")
+      }
+    }
+  } catch (e) {
+    setError("Failed getting inventory data")
+  }
+}
 
-export default function InventoryManagement() {
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+export default function InventoryManagement(props: { setError: Function }) {
+  const [Inventory, setInventory] = useState<Array<{
+    product: ProductResponse
+    stockLevel: StockLevelResponse
+  }> | null>(null)
+  
+  useEffect(() => {
+    getInventoryData(setInventory, props.setError)
+  }, [props.setError])
+
+  if (Inventory === null)
+    return <LoadingComponent msg='Loading Inventory...' />
+
   return (
     <Container>
       <ContainerInner>
-        
       </ContainerInner>
     </Container>
   )
