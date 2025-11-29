c7c89701d4f916291f9c68fece538e83eee8b573
student13123425 <nicolae.m.mihaialexandru24@stud.rau.ro>
2025-11-29 12:16:47 +0200

    Remove private page components and update PrivatePages
    Deleted CRM, Dashboard, InventoryManager, PointOfSale, ProductCatalog, and TransactionHistory components from the Private pages directory. Updated PrivatePages.tsx to remove references to these components and simplify its implementation. This refactor likely prepares for a new structure or redesign of private page functionality.

--- Frontend/src/Pages/Private/PrivatePages.tsx
+++ Frontend/src/Pages/Private/PrivatePages.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-export default function PrivatePages(props:{LoginToken:string,onLogout:Function}) {
+
+export default function PrivatePages({ LoginToken, onLogout }: PrivatePagesProps) {
   return (
-    <div>PrivatePages</div>
-  )
+      <></>
+  );
 }
