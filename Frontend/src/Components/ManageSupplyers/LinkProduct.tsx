31db75dadcfb899a1e993e5b8c81d438be7bb8e6
student13123425 <nicolae.m.mihaialexandru24@stud.rau.ro>
2025-11-30 23:20:48 +0200

    add LinkProduct title

--- Frontend/src/Components/ManageSupplyers/LinkProduct.tsx
+++ Frontend/src/Components/ManageSupplyers/LinkProduct.tsx
@@ -10,10 +10,18 @@ const Container = styled.div`
   overflow: hidden;
 `
 
+const Title = styled.h1`
+  font-size: 2.25rem;
+  font-weight: 800;
+  color: #111827;
+  margin-bottom: 2rem;
+  flex-shrink: 0;
+`
+
 export default function LinkProduct(props:{}) {
   return (
     <Container>
-
+        <Title>Link Product</Title>
     </Container>
   )
 }
