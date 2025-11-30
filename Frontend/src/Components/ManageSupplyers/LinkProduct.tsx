feac8d5baba3fc6f0ac0b1dc4d2ea4f918360f19
student13123425 <nicolae.m.mihaialexandru24@stud.rau.ro>
2025-11-30 23:29:08 +0200

    LinkProduct Component part 1

--- Frontend/src/Components/ManageSupplyers/LinkProduct.tsx
+++ Frontend/src/Components/ManageSupplyers/LinkProduct.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import type { ProductResponse } from '../../script/objects'
 
 const Container = styled.div`
   width: 100vw;
@@ -8,6 +9,7 @@ const Container = styled.div`
   padding: 2rem;
   font-family: 'Inter', sans-serif;
   overflow: hidden;
+  gap: 10px;
 `
 
 const Title = styled.h1`
@@ -18,10 +20,46 @@ const Title = styled.h1`
   flex-shrink: 0;
 `
 
-export default function LinkProduct(props:{}) {
+const Side=styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`
+
+const FormContainer=styled.div`
+    display: flex;
+    gap: 10px;
+`
+
+const Label=styled.div`
+    border-bottom: 2px solid #000;
+    font-size: 1.5rem;
+`
+
+const NrInput=styled.input`
+    
+`
+
+const Btn=styled.div`
+    width: 100%;
+`
+
+export default function LinkProduct(props:{products:ProductResponse[]}) {
   return (
     <Container>
         <Title>Link Product</Title>
+        <FormContainer>
+            <Side>
+                <Label>Supplyer Price</Label>
+                <NrInput type='number'/>
+            </Side>
+            <Side>
+                <Label>Avg Delivery Speed(Days)</Label>
+                <NrInput type='number'/>
+            </Side>
+        </FormContainer>
+        <Btn>Link Product</Btn>
     </Container>
   )
 }
