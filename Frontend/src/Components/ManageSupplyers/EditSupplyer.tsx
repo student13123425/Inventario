7c59efd6a2c9133b2e0fd61c2abeeb599ba8b2f6
student13123425 <nicolae.m.mihaialexandru24@stud.rau.ro>
2025-11-30 13:30:23 +0200

    Add state hooks to EditSupplyer component
    Introduced useState hooks for Name, Email, and Phone fields in the EditSupplyer component to manage supplier data. The component's content was also cleared for further development.

--- Frontend/src/Components/ManageSupplyers/EditSupplyer.tsx
+++ Frontend/src/Components/ManageSupplyers/EditSupplyer.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import type { SupplierResponse } from '../../script/objects'
 
 export default function EditSupplyer(props:{item: SupplierResponse}) {
+  const [Name,setName]=useState(props.item.Name);
+  const [Email,setEmail]=useState(props.item.email);
+  const [Phone,setPhone]=useState(props.item.phone_number);
   return (
-    <div>EditSupplyer</div>
+    <div></div>
   )
 }
